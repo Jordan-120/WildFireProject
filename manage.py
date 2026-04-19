776234e05b97451a260498e0a7cae800b1912a2d
Jordan-120 <jkgilbert@live.ca>
2026-04-19 13:57:09 -0600

    Add files via upload

--- manage.py
+++ manage.py
@@ -1,9 +1,12 @@
+"""Module description."""
+
 #!/usr/bin/env python
 import os
 import sys
 
 
 def main():
+    """Function documentation."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wildfire_site.settings')
     try:
         from django.core.management import execute_from_command_line
